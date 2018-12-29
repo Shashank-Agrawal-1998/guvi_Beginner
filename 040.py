1e0eb97f436056f418e8d8d9cd33ddd7dc0e3f75
Shashank-Agrawal-1998 <44235590+Shashank-Agrawal-1998@users.noreply.github.com>
2018-12-30 03:15:33 +0530

    040 Guvi Program

---
+++ 040.py
@@ -0,0 +1,15 @@
+n = int(input())
+if n==1:
+    print(1)
+elif n==2:
+    print(1,1)
+elif n>2:
+    a = 1
+    b = 1
+    print(1,end=' ')
+    print(1,end=' ')
+    for i in range(n-2):
+        c = a + b
+        a = b
+        b = c
+        print(c, end=' ')
